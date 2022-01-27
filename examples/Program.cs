c504edec975ad3852a262fb7f57f4fec034673e6
Ahad Desktop <me@ahadcove.com>
2022-01-26 22:20:13 -0500

    migrating to new layout

--- examples/Program.cs
+++ examples/Program.cs
@@ -1,100 +1,130 @@
-﻿
-using System;
-using Newtonsoft.Json;
-using Newtonsoft.Json.Linq;
-using System.Threading.Tasks;
+﻿using System;
+using System.Threading;
 
-
-namespace lumia
+namespace Examples
 {
-    class LumiaSdkExample1
+    public class Program
     {
-        private static string token = "4enrnh84scr54d36kyy2"; // Game token that you have retrieved from https://lumiastream.com/games/submit
-        private static string name = ""; // Name of the game that will be prompted for the user to approve
-
-        public static async Task testSends(LumiaSdk lumia)
+        public static void Main(string[] args)
         {
-            // Sending an alert event example
-            await lumia.sendAlert(LumiaSDKAlertValues.TWITCH_FOLLOWER);
+            // Create a new instance of the WebSocket class.
+            //
+            // The WebSocket class inherits the System.IDisposable interface, so you can
+            // use the using statement. And the WebSocket connection will be closed with
+            // close status 1001 (going away) when the control leaves the using block.
+            //
+            // If you would like to connect to the server with the secure connection,
+            // you should create a new instance with a wss scheme WebSocket URL.
 
-            // Sending a command; with a callback to get the result for this call
-            await lumia.sendCommand("red", null, null);
-
-            RGB rgb = new RGB();
-            rgb.r = 255;
-            rgb.g = 0;
-            rgb.b = 255;
-
-            await lumia.sendColor(rgb, 60, 1000);
-
-            // Sending a brightness
-            await lumia.sendBrightness(100);
-
-            // Sending a TTS message
-            await lumia.sendTts("This SDK is the best");
-
-            // Sending a Chat bot message
-            await lumia.sendChatbot(Platforms.TWITCH, "This SDK is the best");
-
-            // Sending a raw event example
-            ILumiaSdkSendPack pack = new ILumiaSdkSendPack();
-            pack.type = LumiaSDKCommandTypes.ALERT;
-            pack.params_ = new LumiaSDKPackParams();
-            pack.params_.value = LumiaSDKAlertValues.TWITCH_FOLLOWER;
-            await lumia.send(pack);
-        }
-        public static async Task run()
-        {
-            LumiaSdk lumia = new LumiaSdk();
-            await lumia.init(token, name);
-
-            lumia.error += (string r) =>
+            using (var nf = new Notifier())
+            using (var ws = new WebSocket("ws://echo.websocket.org"))
+            //using (var ws = new WebSocket ("wss://echo.websocket.org"))
+            //using (var ws = new WebSocket ("ws://localhost:4649/Echo"))
+            //using (var ws = new WebSocket ("wss://localhost:5963/Echo"))
+            //using (var ws = new WebSocket ("ws://localhost:4649/Echo?name=nobita"))
+            //using (var ws = new WebSocket ("wss://localhost:5963/Echo?name=nobita"))
+            //using (var ws = new WebSocket ("ws://localhost:4649/Chat"))
+            //using (var ws = new WebSocket ("wss://localhost:5963/Chat"))
+            //using (var ws = new WebSocket ("ws://localhost:4649/Chat?name=nobita"))
+            //using (var ws = new WebSocket ("wss://localhost:5963/Chat?name=nobita"))
             {
-                Console.WriteLine("error : " + r);
-            };
+                // Set the WebSocket events.
 
-            lumia.closed += (string r) =>
-            {
-                Console.WriteLine("closed : " + r);
-            };
+                ws.OnOpen += (sender, e) => ws.Send("Hi, there!");
 
+                ws.OnMessage += (sender, e) =>
+                    nf.Notify(
+                      new NotificationMessage
+                      {
+                          Summary = "WebSocket Message",
+                          Body = !e.IsPing ? e.Data : "Received a ping.",
+                          Icon = "notification-message-im"
+                      }
+                    );
 
-            lumia.events += (JObject data) =>
-            {
-                Console.WriteLine("Event data : " + data.ToString());
+                ws.OnError += (sender, e) =>
+                    nf.Notify(
+                      new NotificationMessage
+                      {
+                          Summary = "WebSocket Error",
+                          Body = e.Message,
+                          Icon = "notification-message-im"
+                      }
+                    );
 
+                ws.OnClose += (sender, e) =>
+                    nf.Notify(
+                      new NotificationMessage
+                      {
+                          Summary = String.Format("WebSocket Close ({0})", e.Code),
+                          Body = e.Reason,
+                          Icon = "notification-message-im"
+                      }
+                    );
+#if DEBUG
+        // To change the logging level.
+        ws.Log.Level = LogLevel.Trace;
 
-                // here we give the context as we know it's an SDK Eent types
-                switch (LumiaUtils.getTypeValueFromString<LumiaSdkEventTypes>("LumiaSdkEventTypes", data["type"].Value<string>()))
+        // To change the wait time for the response to the Ping or Close.
+        //ws.WaitTime = TimeSpan.FromSeconds (10);
+
+        // To emit a WebSocket.OnMessage event when receives a ping.
+        //ws.EmitOnPing = true;
+#endif
+                // To enable the Per-message Compression extension.
+                //ws.Compression = CompressionMethod.Deflate;
+
+                // To validate the server certificate.
+                /*
+                ws.SslConfiguration.ServerCertificateValidationCallback =
+                  (sender, certificate, chain, sslPolicyErrors) => {
+                    ws.Log.Debug (
+                      String.Format (
+                        "Certificate:\n- Issuer: {0}\n- Subject: {1}",
+                        certificate.Issuer,
+                        certificate.Subject
+                      )
+                    );
+
+                    return true; // If the server certificate is valid.
+                  };
+                 */
+
+                // To send the credentials for the HTTP Authentication (Basic/Digest).
+                //ws.SetCredentials ("nobita", "password", false);
+
+                // To send the Origin header.
+                //ws.Origin = "http://localhost:4649";
+
+                // To send the cookies.
+                //ws.SetCookie (new Cookie ("name", "nobita"));
+                //ws.SetCookie (new Cookie ("roles", "\"idiot, gunfighter\""));
+
+                // To connect through the HTTP Proxy server.
+                //ws.SetProxy ("http://localhost:3128", "nobita", "password");
+
+                // To enable the redirection.
+                //ws.EnableRedirection = true;
+
+                // Connect to the server.
+                ws.Connect();
+
+                // Connect to the server asynchronously.
+                //ws.ConnectAsync ();
+
+                Console.WriteLine("\nType 'exit' to exit.\n");
+                while (true)
                 {
-                    case LumiaSdkEventTypes.STATES:
-                        Console.WriteLine("States have been updated:  " + data.ToString());
+                    Thread.Sleep(1000);
+                    Console.Write("> ");
+                    var msg = Console.ReadLine();
+                    if (msg == "exit")
                         break;
 
-                    case LumiaSdkEventTypes.COMMAND:
-                        Console.WriteLine("A Chat Command is being triggered:  " + data.ToString());
-                        break;
-
-                    case LumiaSdkEventTypes.CHAT:
-                        Console.WriteLine("New chat message:  " + data.ToString());
-                        break;
-
-                    case LumiaSdkEventTypes.ALERT:
-                        Console.WriteLine("New alert:  " + data.ToString());
-                        break;
+                    // Send a text message.
+                    ws.Send(msg);
                 }
-            };
-
-            var r = await lumia.getInfo();
-
-            Console.WriteLine("get info result : " + r.ToString());
-
-            await testSends(lumia);
-
-        }
-        static void Main(string[] args)
-        {
-            run().GetAwaiter().GetResult();
+            }
         }
     }
 }
